06a583e41c6e9356c8a33f4651dcb39cf44b4955
harishgattamaneni <103264547+harishgattamaneni@users.noreply.github.com>
2025-09-15 16:42:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function insert_Row() {
-    let ele= document.getElementById("sampleTable")
-    let n=ele.childElementCount
-    let newTr=document.createElement("tr")
-    let newTd1=document.createElement("td")
-    let newTd2=document.createElement("td")
-    newTd1.innerHTML=`New Cell1`
-    newTd2.innerHTML=`New Cell2`
-    newTr.appendChild(newTd1)
-    newTr.appendChild(newTd2)
-    ele.innerHTML=newTr.innerHTML+ele.innerHTML
+    let ele = document.getElementById("sampleTable");
+    let newTr = document.createElement("tr");
+    let newTd1 = document.createElement("td");
+    let newTd2 = document.createElement("td");
+    
+    newTd1.innerHTML = `New Cell1`;
+    newTd2.innerHTML = `New Cell2`;
+    
+    newTr.appendChild(newTd1);
+    newTr.appendChild(newTd2);
+    
+    // Instead of changing innerHTML, use appendChild
+    ele.insertBefore(newTr, ele.firstChild);
 }
